24b14bf00be618ae6400323a3f6f1e9dd758e215
Dillon Goetz <dgoetzcodes@gmail.com>
2024-10-29 23:27:21 -0400

    Create 10-29-24 maxchar.js

---
+++ 5-kyu/10-29-24 maxchar.js
@@ -0,0 +1,23 @@
+// return the string with the letter with the most occurences
+
+
+function maxCharacter(str){
+    //map + count + max char
+    let charMap = {}
+    count = 0,
+    maxChar = null
+
+    //map for chars in str + freq
+    for (const chat of str){
+        charMap[char] = charMap[char] + 1 || 1
+    }
+
+    //loop and find most freq char
+    for (const char in charMap){
+        if (charMap[char] > count){
+            count = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
